9fd2896a558ffd8e95d21bb7b0de0213cdf64359
Dmitry Takmakov <dj-dat@yandex.ru>
2020-02-04 04:09:31 +0500

    second task and text file for it

---
+++ task02.py
@@ -0,0 +1,7 @@
+line_counter = 0
+word_counter = 0
+with open('task2.txt', 'r') as file:
+    for line in file:
+        word_counter = line.count(' ') + 1
+        line_counter += 1
+        print(f'В строке {line_counter} - {word_counter} слов')
