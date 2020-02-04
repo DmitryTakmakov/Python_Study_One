26033376e310e37177cd330c2308055927e36cf2
Dmitry Takmakov <dj-dat@yandex.ru>
2020-02-04 05:29:29 +0500

    third task and text file for it. also - a comment in previous task

--- task02.py
+++ task02.py
@@ -2,6 +2,6 @@ line_counter = 0
 word_counter = 0
 with open('task2.txt', 'r') as file:
     for line in file:
-        word_counter = line.count(' ') + 1
+        word_counter = line.count(' ') + 1  # считаем количество слов как кол-во пробелов + 1
         line_counter += 1
         print(f'В строке {line_counter} - {word_counter} слов')
