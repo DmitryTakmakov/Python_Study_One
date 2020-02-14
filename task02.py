ce859b4d251ce1a47aaec4f93a27c81afd7f2a9b
Dmitry Takmakov <dj-dat@yandex.ru>
2020-02-15 02:01:52 +0500

    rewritten task two

--- task02.py
+++ task02.py
@@ -14,9 +14,15 @@ class Coat(Clothes):
 
     @property
     def fabric_volume(self):
-        v = float(input('Введите размер пальто.\n'))
-        fabric_volume = (v / 6.5) + 0.5
-        return f'Чтобы сшить {self.name} такого размера потребуется {fabric_volume.__round__(2)} м. ткани.'
+        while True:
+            try:
+                v = float(input('Введите размер пальто.\n'))
+            except ValueError:
+                print('Вы ввели недопустимое значение, попробуйте еще раз. Размер нужно вводить в цифрах, через точку.')
+            else:
+                fabric_volume = (v / 6.5) + 0.5
+                break
+        return fabric_volume.__round__(2)
 
 
 class Suit(Clothes):
@@ -25,15 +31,22 @@ class Suit(Clothes):
 
     @property
     def fabric_volume(self):
-        h = float(input('Введите рост в метрах.\n'))
-        fabric_volume = (2 * h) + 0.3
-        return f'Чтобы сшить {self.name} на такой рост потребуется {fabric_volume.__round__(2)} м. ткани.'
+        while True:
+            try:
+                h = float(input('Введите рост в метрах.\n'))
+            except ValueError:
+                print('Вы ввели недопустимое значение. Рост нужно вводить в цифрах, через точку. Попробуйте еще раз.')
+            else:
+                fabric_volume = (2 * h) + 0.3
+                break
+        return fabric_volume.__round__(2)
 
 
 if __name__ == '__main__':
     c1 = Coat()
     c2 = Suit()
     vol1 = c1.fabric_volume
-    print(vol1)
+    print(f'На такое пальто потребуется {vol1} м. ткани.')
     vol2 = c2.fabric_volume
-    print(vol2)
+    print(f'На такой костюм потребуется {vol2} м. ткани.')
+    print(f'Всего потребуется {(vol1 + vol2).__round__(2)} м. ткани.')
